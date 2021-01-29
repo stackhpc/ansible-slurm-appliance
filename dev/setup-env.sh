30a0e6f7c4c069d210f68278dd3558f3f8d898a1
Will Szumski <will@stackhpc.com>
2021-01-29 19:25:41 +0000

    Add github-action to run vagrant example

---
+++ dev/setup-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 -m venv venv
+. venv/bin/activate
+pip install -U pip
+pip install -U setuptools
+pip install -r requirements.txt
+# Install ansible dependencies ...
+ansible-galaxy role install -r requirements.yml -p ansible/roles
+ansible-galaxy collection install -r requirements.yml -p ansible/collections
