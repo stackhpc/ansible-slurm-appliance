781f1ec5528fd726554404de0f2e2bfe7ebdc5d1
Will Szumski <will@stackhpc.com>
2021-02-12 19:28:54 +0000

    Add workaround for CI
    Seeing this issue:
    
    https://github.com/rbenv/ruby-build/issues/1385
    
    mid ansible run.

--- dev/vagrant-example-configure.sh
+++ dev/vagrant-example-configure.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
+# Workaround for: objc[3705]: +[__NSCFConstantString initialize] may have been in progress in another thread when fork() was called.
+# in CI
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 echo "Activating environment"
 
 set +u
