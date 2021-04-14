0ce908a4acabcfd3836f62e6968e63c4578c02a1
Steve Brasier <steveb@stackhpc.com>
2021-04-14 17:05:50 +0000

    gitignore ansible vault password

---
+++ environments/nrel-proto-vm/terraform/main.tf
@@ -0,0 +1,217 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+variable "compute_names" {
+    type = list(string)
+    default = ["compute-0", "compute-1"]
+    description = "A list of hostnames for the compute nodes (will be prefixed by cluster_name)"
+}
+
+variable "login_names" {
+  type = list(string)
+  default = ["login-0"]
+  description = "A list of hostnames for the login nodes (will be prefixed by cluster_name)"
+}
+
+variable "cluster_name" {
+    type = string
+    description = "Name for cluster, used as prefix for resources"
+}
+
+variable "key_pair" {
+    type = string
+    description = "Name of an existing keypair in OpenStack"
+}
+
+variable "login_flavor" {
+    type = string
+    description = "Name of instance flavor for login node(s)"
+}
+
+variable "login_image" {
+    type = string
+    description = "Name of image for login node(s)"
+}
+
+variable "control_flavor" {
+    type = string
+    description = "Name of instance flavor for control node"
+}
+
+variable "control_image" {
+    type = string
+    description = "Name of image for compute node"
+}
+
+variable "compute_flavor" {
+    type = string
+    description = "Name of instance flavor for compute node(s)"
+}
+
+variable "compute_image" {
+    type = string
+    description = "Name of image for compute node(s)"
+}
+
+variable "cidr" {
+    type = string
+    default = "192.168.42.0/24"
+    description = "Range in CIDR notation for cluster subnet"
+}
+
+variable "external_network" {
+  type = string
+  description = "Name of external network"
+}
+
+variable "external_router" {
+  type = string
+  description = "Name of router on external network"
+}
+
+variable "provider_networks" {
+  type = list(string)
+  description = "Name of pre-existing networks to additionally connect to cluster instances"
+  default = []
+}
+
+data "openstack_networking_network_v2" "external_network" {
+  name = var.external_network
+}
+
+data "openstack_networking_network_v2" "provider_networks" {
+  for_each = toset(var.provider_networks)
+
+  name = each.key
+}
+
+resource "openstack_networking_network_v2" "cluster" {
+    name           = var.cluster_name
+    admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "cluster" {
+    name            = var.cluster_name
+    network_id      = openstack_networking_network_v2.cluster.id
+    cidr            = var.cidr
+    ip_version      = 4
+}
+
+resource "openstack_compute_instance_v2" "control" {
+  name = "${var.cluster_name}-control"
+  image_name = var.control_image
+  flavor_name = var.control_flavor
+  key_pair = var.key_pair
+  
+  network {
+    uuid = openstack_networking_network_v2.cluster.id
+  }
+
+  dynamic "network" {
+    for_each = data.openstack_networking_network_v2.provider_networks
+  
+    content {
+      uuid = network.value["id"]
+    }
+  }
+
+  timeouts {
+    create = "60m"
+    delete = "20m"
+  }
+
+}
+
+resource "openstack_compute_instance_v2" "logins" {
+
+  for_each = toset(var.login_names)
+
+  name = "${var.cluster_name}-${each.value}"
+  image_name = var.login_image
+  flavor_name = var.login_flavor
+  key_pair = var.key_pair
+
+  network {
+    uuid = openstack_networking_network_v2.cluster.id
+  }
+
+  dynamic "network" {
+    for_each = data.openstack_networking_network_v2.provider_networks
+  
+    content {
+      uuid = network.value["id"]
+    }
+  }
+
+  timeouts {
+    create = "60m"
+    delete = "20m"
+  }
+
+}
+
+resource "openstack_compute_instance_v2" "compute" {
+
+  for_each = toset(var.compute_names)
+
+  name = "${var.cluster_name}-${each.value}"
+  image_name = var.compute_image
+  flavor_name = var.compute_flavor
+  key_pair = var.key_pair
+
+  network {
+    uuid = openstack_networking_network_v2.cluster.id
+  }
+
+  dynamic "network" {
+    for_each = data.openstack_networking_network_v2.provider_networks
+  
+    content {
+      uuid = network.value["id"]
+    }
+  }
+
+  timeouts {
+    create = "60m"
+    delete = "20m"
+  }
+
+}
+
+data "openstack_networking_router_v2" "external" {
+  name                = var.external_router
+}
+
+resource "openstack_networking_router_interface_v2" "cluster" {
+  router_id = data.openstack_networking_router_v2.external.id
+  subnet_id = openstack_networking_subnet_v2.cluster.id
+}
+
+resource "openstack_networking_floatingip_v2" "login" {
+  pool = data.openstack_networking_network_v2.external_network.name
+}
+
+resource "openstack_compute_floatingip_associate_v2" "login" {
+  floating_ip = openstack_networking_floatingip_v2.login.address
+  instance_id = openstack_compute_instance_v2.login.id
+}
+
+# TODO: needs fixing for case where creation partially fails resulting in "compute.network is empty list of object"
+resource "local_file" "hosts" {
+  content  = templatefile("${path.module}/inventory.tpl",
+                          {
+                            "cluster_name": var.cluster_name
+                            "control": openstack_compute_instance_v2.control,
+                            "logins": openstack_compute_instance_v2.logins,
+                            "computes": openstack_compute_instance_v2.compute,
+                            # "fip": openstack_networking_floatingip_v2.login
+                          },
+                          )
+  filename = "../inventory/hosts"
+}
---
+++ environments/nrel-proto-vm/terraform/terraform.tfvars
@@ -0,0 +1,17 @@
+compute_names = ["hpc-0", "hpc-1", "hpc-2", "hpc-3", "express-0", "express-1"]
+login_names = ["login-0", "login-1"]
+cluster_name  = "proto" # don't put dashes (creates invalid ansible group names) or underscores (creates hostnames which get mangled) in this
+key_pair = "centos_at_deploy"
+#provider_networks = ["nrel-storage"] # TODO: activate this on vermilion - can't demo on vglabs baremetal nodes
+cidr = "192.168.42.0/24"
+external_network = "external"
+external_router = "external"
+
+login_image = "CentOS8.3-cloud"
+login_flavor = "baremetal"
+
+control_image = "CentOS8.3-cloud"
+control_flavor = "baremetal"
+
+compute_image = "CentOS8.3-cloud"
+compute_flavor = "baremetal"
