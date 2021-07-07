95ad6132d1ecf9bc5f40f39de9d6cbc1707907ec
Steve Brasier <steveb@stackhpc.com>
2021-07-07 13:50:24 +0000

    wip - autogenerate slurm partitions from compute node types

--- environments/nrel-workshop/terraform/inventory.tf
+++ environments/nrel-workshop/terraform/inventory.tf
@@ -3,8 +3,10 @@ resource "local_file" "inventory" {
                           {
                             "cluster_name": var.cluster_name
                             "control": openstack_compute_instance_v2.control,
-                            "logins": openstack_compute_instance_v2.logins,
-                            "computes": openstack_compute_instance_v2.computes,
+                            "logins": openstack_compute_instance_v2.login,
+                            "computes": openstack_compute_instance_v2.compute,
+                            "compute_types": var.compute_types,
+                            "compute_nodes": var.compute_nodes,
                           },
                           )
   filename = "../inventory/hosts"
--- environments/nrel-workshop/terraform/inventory.tpl
+++ environments/nrel-workshop/terraform/inventory.tpl
@@ -14,3 +14,11 @@ ${login.name} ansible_host=${[for n in login.network: n.fixed_ip_v4 if n.access_
 %{ for compute in computes ~}
 ${compute.name} ansible_host=${[for n in compute.network: n.fixed_ip_v4 if n.access_network][0]} server_networks='${jsonencode({for net in compute.network: net.name => [ net.fixed_ip_v4 ] })}'
 %{ endfor ~}
+
+# Define groups for slurm parititions:
+%{ for compute_type in compute_types ~}
+[${cluster_name}_${compute_type}]
+    %{ for nodename, nodetype in compute_nodes ~}
+%{ if nodetype == compute_type }${cluster_name}-${nodename}%{ endif }
+    %{ endfor ~}
+%{ endfor ~}
