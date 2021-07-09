7eb41ec40dbd4cbd52f4fbff105fcf510655c29d
Steve Brasier <steveb@stackhpc.com>
2021-07-09 14:57:04 +0000

    autogenerate slurm partitions from compute groups

--- environments/nrel-workshop/terraform/inventory.tf
+++ environments/nrel-workshop/terraform/inventory.tf
@@ -1,4 +1,4 @@
-resource "local_file" "inventory" {
+resource "local_file" "hosts" {
   content  = templatefile("${path.module}/inventory.tpl",
                           {
                             "cluster_name": var.cluster_name
@@ -10,4 +10,15 @@ resource "local_file" "inventory" {
                           },
                           )
   filename = "../inventory/hosts"
+}
+
+locals {
+  openhpc_slurm_partitions = {
+    openhpc_slurm_partitions: [for name, _ in var.compute_types : {"name": name}]
+  }
+}
+
+resource "local_file" "slurm_partitions" {
+  content  = yamlencode(local.openhpc_slurm_partitions)
+  filename = "../inventory/group_vars/openhpc/partitions.yml"
 }
