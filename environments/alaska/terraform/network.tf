440ea75d97ab77c71b6b7004fe0ea041d6ecdb81
Steve Brasier <steveb@stackhpc.com>
2021-08-17 11:21:49 +0000

    move cluster to iris-alaska-prod-internal network

--- environments/alaska/terraform/network.tf
+++ environments/alaska/terraform/network.tf
@@ -1,8 +1,8 @@
 
 data "openstack_networking_network_v2" "cluster_net" {
-  name           = "WCDC-iLab-60"
+  name           = "iris-alaska-prod-internal"
 }
 
 data "openstack_networking_subnet_v2" "cluster_subnet" {
-  name            = "WCDC-iLab-60"
+  name            = "iris-alaska-prod-internal"
 }
