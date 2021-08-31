87d9912880ec948e40b3b4465bd11b08ace7290c
Steve Brasier <steveb@stackhpc.com>
2021-08-31 16:39:08 +0000

    remove config drive use from TF

--- environments/alaska-common/terraform/modules/cluster/nodes.tf
+++ environments/alaska-common/terraform/modules/cluster/nodes.tf
@@ -35,7 +35,6 @@ resource "openstack_compute_instance_v2" "login" {
   image_name = each.value.image
   flavor_name = each.value.flavor
   key_pair = var.key_pair
-  config_drive = true
   security_groups = ["default", "SSH"]
 
   network {
@@ -55,7 +54,6 @@ resource "openstack_compute_instance_v2" "compute" {
   image_name = lookup(var.compute_images, each.key, var.compute_types[each.value].image)
   flavor_name = var.compute_types[each.value].flavor
   key_pair = var.key_pair
-  // config_drive = true
 
   network {
     uuid = data.openstack_networking_subnet_v2.cluster_subnet.network_id
