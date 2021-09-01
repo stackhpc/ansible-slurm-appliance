6624d1a4016d3bb9f66475fe6244fe46900fd55b
Steve Brasier <steveb@stackhpc.com>
2021-09-01 16:08:53 +0000

    add dev environment

---
+++ environments/alaska-dev/terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+module "cluster" {
+    source = "../../alaska-common/terraform/modules/cluster"
+    cluster_name = "alaska"
+    key_pair = "centos-slurm-deploy"
+    cluster_net = "iris-alaska-dev-internal"
+    cluster_subnet = "iris-alaska-dev-internal"
+    // rdma_net = "WCDC-iLab-60"
+    home_volume = "alaska-dev-home"
+    slurmctld_volume = "alaska-dev-slurmctld"
+    slurmctl_port = "alaska-dev-slurmctl"
+    compute_nodes = {
+      compute-0: "small"
+      compute-1: "small"
+      compute-2: "small"
+      compute-3: "small"
+    }
+    login_nodes = {
+      login-0: {
+        flavor: "vm.alaska.cpu.himem.quarter"
+        image: "CentOS8-2105"
+        address: "128.232.222.246"
+      }
+    }
+}
