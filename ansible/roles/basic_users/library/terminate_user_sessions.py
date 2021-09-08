728caa4a4ea63124bba80f27b9f709d38563fd13
Steve Brasier <steveb@stackhpc.com>
2021-09-08 15:19:08 +0000

    add docs for terminate_user_sessions

--- ansible/roles/basic_users/library/terminate_user_sessions.py
+++ ansible/roles/basic_users/library/terminate_user_sessions.py
@@ -7,66 +7,32 @@ __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
-module: my_test
+module: terminate_user_sessions
 
-short_description: This is my test module
+short_description: Terminate systemd user sessions
 
 # If this is part of a collection, you need to use semantic versioning,
 # i.e. the version is of the form "2.5.0" and not "2.4".
 version_added: "1.0.0"
 
-description: This is my longer description explaining my test module.
+description: Terminate systemd user sessions.
 
 options:
-    name:
-        description: This is the message to send to the test module.
+    user:
+        description: Name of user
         required: true
         type: str
-    new:
-        description:
-            - Control to demo if the result of this module is changed or not.
-            - Parameter description can be a list as well.
-        required: false
-        type: bool
-# Specify this value according to your collection
-# in format of namespace.collection.doc_fragment_name
-extends_documentation_fragment:
-    - my_namespace.my_collection.my_doc_fragment_name
-
+    
 author:
-    - Your Name (@yourGitHubHandle)
+    - Steve Brasier (stackhpc.com)
 '''
 
 EXAMPLES = r'''
-# Pass in a message
-- name: Test with a message
-  my_namespace.my_collection.my_test:
-    name: hello world
-
-# pass in a message and have changed true
-- name: Test with a message and changed output
-  my_namespace.my_collection.my_test:
-    name: hello world
-    new: true
-
-# fail the module
-- name: Test failure of the module
-  my_namespace.my_collection.my_test:
-    name: fail me
+- terminate_user_sessions:
+    name: fred
 '''
 
 RETURN = r'''
-# These are examples of possible return values, and in general should use other names for return values.
-original_message:
-    description: The original name param that was passed in.
-    type: str
-    returned: always
-    sample: 'hello world'
-message:
-    description: The output message that the test module generates.
-    type: str
-    returned: always
-    sample: 'goodbye'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
