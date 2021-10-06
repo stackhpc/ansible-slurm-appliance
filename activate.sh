c876d2ffa74f6214731ded0fdab1ff7391b9fb6e
Steve Brasier <steveb@stackhpc.com>
2021-10-07 04:20:37 +1300

    use same vault password file as rest of deployment

--- activate.sh
+++ activate.sh
@@ -1,4 +1,4 @@
 . venv/bin/activate
 . environments/nesi-accept/activate
 PATH=${PATH}:/home/sbra208/nesi-slurm-accept/dev
-export ANSIBLE_VAULT_PASSWORD_FILE=~/vault-password.txt
+export ANSIBLE_VAULT_PASSWORD_FILE=~/deployment/vault.pass
