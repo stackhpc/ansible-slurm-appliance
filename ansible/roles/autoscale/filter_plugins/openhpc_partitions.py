1ee10e9931c9478a48922d09d6d5495a647515fc
Steve Brasier <steveb@stackhpc.com>
2021-10-06 13:09:06 +0000

    fix actually generataing cloud_node info

--- ansible/roles/autoscale/filter_plugins/openhpc_partitions.py
+++ ansible/roles/autoscale/filter_plugins/openhpc_partitions.py
@@ -21,28 +21,32 @@ import re
 
 REQUIRED_INSTANCE_ATTRS=('flavor', 'image', 'keypair', 'network')
 
-def modify_autoscale_partitions(partitions):
+def modify_autoscale_partitions(partitions, flavors):
     """ TODO: docs
     """
 
     for part in partitions:
         for group in part.get('groups', [part]):
             group_name = group.get('name', '<undefined>')
-            extra_nodes = group.get('extra_nodes', [])
             
             if 'cloud_nodes' in group:
                 if 'cloud_instances' not in group:
                     raise errors.AnsibleFilterError(f"`openhpc_slurm_partitions` group '{group_name}' specifies 'cloud_nodes' but is missing 'cloud_instances'.")
-                missing_attrs = ','.join(set(REQUIRED_INSTANCE_ATTRS).difference(group['cloud_instances']))
+                missing_attrs = ', '.join(set(REQUIRED_INSTANCE_ATTRS).difference(group['cloud_instances']))
                 if missing_attrs:
                     raise errors.AnsibleFilterError(f"`openhpc_slurm_partitions` group '{group_name}' item 'cloud_instances' is missing items: {missing_attrs}.")
                 cloud_names = group['cloud_nodes']
                 # TODO: check for cloud nodes overlapping real ones?
                 
                 features = ['%s=%s' % (k, v) for (k, v) in group['cloud_instances'].items()]
-                cloud_nodes = f'NodeName={cloud_names} State=CLOUD Features={features}'
-
-                extra_nodes.append(cloud_nodes)
+                cloud_nodes = {
+                    'NodeName': cloud_names,
+                    'State':'CLOUD',
+                    'Features': ','.join(features),
+                }
+                
+                group['extra_nodes'] = group.get('extra_nodes', [])
+                group['extra_nodes'].append(cloud_nodes)
 
     return partitions
 
