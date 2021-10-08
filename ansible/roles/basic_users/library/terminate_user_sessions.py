3765fb8e12c1367f4c7ddfbac3cc0f41adb5c521
Steve Brasier <steveb@stackhpc.com>
2021-10-08 10:24:37 +0000

    fix merge from main

--- ansible/roles/basic_users/library/terminate_user_sessions.py
+++ ansible/roles/basic_users/library/terminate_user_sessions.py
@@ -15,60 +15,6 @@ short_description: Terminate systemd user sessions
 # i.e. the version is of the form "2.5.0" and not "2.4".
 version_added: "1.0.0"
 
-<<<<<<< HEAD
-description: This is my longer description explaining my test module.
-
-options:
-    name:
-        description: This is the message to send to the test module.
-        required: true
-        type: str
-    new:
-        description:
-            - Control to demo if the result of this module is changed or not.
-            - Parameter description can be a list as well.
-        required: false
-        type: bool
-# Specify this value according to your collection
-# in format of namespace.collection.doc_fragment_name
-extends_documentation_fragment:
-    - my_namespace.my_collection.my_doc_fragment_name
-
-author:
-    - Your Name (@yourGitHubHandle)
-'''
-
-EXAMPLES = r'''
-# Pass in a message
-- name: Test with a message
-  my_namespace.my_collection.my_test:
-    name: hello world
-
-# pass in a message and have changed true
-- name: Test with a message and changed output
-  my_namespace.my_collection.my_test:
-    name: hello world
-    new: true
-
-# fail the module
-- name: Test failure of the module
-  my_namespace.my_collection.my_test:
-    name: fail me
-'''
-
-RETURN = r'''
-# These are examples of possible return values, and in general should use other names for return values.
-original_message:
-    description: The original name param that was passed in.
-    type: str
-    returned: always
-    sample: 'hello world'
-message:
-    description: The output message that the test module generates.
-    type: str
-    returned: always
-    sample: 'goodbye'
-=======
 description: Terminate systemd user sessions.
 
 options:
@@ -87,7 +33,6 @@ EXAMPLES = r'''
 '''
 
 RETURN = r'''
->>>>>>> main
 '''
 
 from ansible.module_utils.basic import AnsibleModule
