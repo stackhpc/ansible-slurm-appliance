b5a8735de0b967886cd2cd0ec6a3c2004a7f3877
Steve Brasier <steveb@stackhpc.com>
2022-01-12 16:03:05 +0000

    add smslabs-example environemtn

---
+++ environments/smslabs-example/terraform/variables.tf
@@ -0,0 +1,78 @@
+variable "cluster_name" {
+    type = string
+    description = "Name for cluster, used as prefix for resources"
+    default = "ci"
+}
+
+variable "cluster_net" {
+    type = string
+    description = "Name of existing cluster network"
+    default = "stackhpc-ipv4-geneve"
+}
+
+variable "cluster_subnet" {
+    type = string
+    description = "Name of existing cluster subnet"
+    default = "stackhpc-ipv4-geneve-subnet"
+}
+
+variable "key_pair" {
+    type = string
+    description = "Name of an existing keypair in OpenStack"
+    default = "slurm-app-ci"
+}
+
+variable "control_node" {
+    type = map
+    description = "Mapping {flavor: flavor_name, image: image_name_or_id }"
+    default = {
+        flavor: "general.v1.small"
+        image: "Rocky-8-GenericCloud-8.5-20211114.2.x86_64"
+    }
+}
+
+variable "login_nodes" {
+  type = map
+  description = "Mapping defining login nodes: key -> (str) nodename suffix, value -> mapping  {flavor: flavor_name, image: image_name_or_id }"
+  default = {
+      login-0: {
+        flavor: "general.v1.small"
+        image: "Rocky-8-GenericCloud-8.5-20211114.2.x86_64"
+      }
+    }
+}
+
+variable "compute_types" {
+    type = map
+    description = "Mapping defining types of compute nodes: key -> (str) name of type, value -> mapping {flavor: flavor_name, image: image_name_or_id }"
+    default = {
+      small: {
+          flavor: "general.v1.small"
+          image: "Rocky-8-GenericCloud-8.5-20211114.2.x86_64"
+      }
+    }
+}
+
+variable "compute_nodes" {
+    type = map(string)
+    description = "Mapping of compute nodename suffix -> key in compute_types"
+    default = {
+        compute-0: "small"
+        compute-1: "small"
+    }
+}
+
+variable "compute_images" {
+    type = map(string)
+    default = {}
+    description = "Mapping to override compute images from compute_types: key ->(str) node name, value -> (str) image name"
+}
+
+variable "jumphost" {
+    type = map(string)
+    description = "Details of jumphost to use to access nodes, see default for contents"
+    default = {
+        ip: "185.45.78.150"
+        user: "slurm-app-ci"
+    }
+}
