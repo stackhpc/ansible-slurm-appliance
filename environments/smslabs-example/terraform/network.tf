a4cf0a9e2d86e1615ae86c5ecc863bce83d8171f
Steve Brasier <33413598+sjpb@users.noreply.github.com>
2022-01-20 16:26:56 +0000

    Support Rocky 8.5 (#134)
    * support Rocky 8.5
    * require Rocky-compatible cloudalchemy node_exporter & prometheus roles
    * require stackhpc.openhpc supporting Rocky Linux
    * default to Rocky 8.5 images
    * Add openstack-based CI (#137) using sms-labs environment

---
+++ environments/smslabs-example/terraform/network.tf
@@ -0,0 +1,9 @@
+
+data "openstack_networking_network_v2" "cluster_net" {
+  name           = var.cluster_net
+}
+
+data "openstack_networking_subnet_v2" "cluster_subnet" {
+
+  name            = var.cluster_subnet
+}
