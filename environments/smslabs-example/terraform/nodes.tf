c0370d6ff81bfaf0f0c1c6d31afe114eeb6ad46d
Steve Brasier <steveb@stackhpc.com>
2022-02-17 10:17:38 +0000

    fix security groups in smslabs for idempotency

--- environments/smslabs-example/terraform/nodes.tf
+++ environments/smslabs-example/terraform/nodes.tf
@@ -6,7 +6,7 @@ resource "openstack_compute_instance_v2" "control" {
   flavor_name = var.control_node.flavor
   key_pair = var.key_pair
   config_drive = true
-  security_groups = ["default", "ssh"]
+  security_groups = ["default", "SSH"]
 
   network {
     uuid = data.openstack_networking_subnet_v2.cluster_subnet.network_id # ensures nodes not created till subnet created
@@ -24,7 +24,7 @@ resource "openstack_compute_instance_v2" "login" {
   flavor_name = each.value.flavor
   key_pair = var.key_pair
   config_drive = true
-  security_groups = ["default", "ssh"]
+  security_groups = ["default", "SSH"]
 
   network {
     uuid = data.openstack_networking_subnet_v2.cluster_subnet.network_id
