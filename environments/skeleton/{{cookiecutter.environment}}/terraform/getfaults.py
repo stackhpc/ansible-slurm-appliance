a287a99c88e3590c324211373d6bc936c1dcaf26
Steve Brasier <33413598+sjpb@users.noreply.github.com>
2022-05-16 15:29:46 +0100

    Use Arcus for CI (#182)
    * copy smslabs TF to cookiecutter skeleton
    
    * use skeleton TF for arcus
    
    * define ports in skeleton TF
    
    * add direct-mode ports for arcus env
    
    * copy getfaults.py from smslabs to skeleton
    
    * getfaults takes TF state directory
    
    * make security groups idempotent for TF apply
    
    * add arcus env config
    
    * add arcus CI workflow
    
    * temporarily disable smslabs CI during arcus CI development
    
    * move arcus to same base image as smslabs
    
    * bugfix reading TF state for provisioning errors
    
    * automate slurm partition definition
    
    * fix arcus bastion definition for CI
    
    * try to fix ansible  transfer mechanism failing after login rebuild
    
    * use latest base image openhpc-220504-0904 in arcus
    
    * try to fix 'Connection timed out during banner exchange' after login rebuild
    
    * use port security groups in skeleton TF
    
    * revert reset connection in CI
    
    * try to fix 'Connection timed out during banner exchange' after login rebuild
    
    * try to fix 'Connection timed out during banner exchange' after login rebuild
    
    * try to fix 'wait for login' failing login rebuild
    
    * fix pingmatrix HTML format division by zero
    
    * move all github workflows to smslabs.yml
    
    * move workflows to stackhpc.yml
    
    * allow CI runs on clouds to continue if one fails
    
    * move smslabs to use common terraform (as does arcus)
    
    * update README status badge to correct workflow
    
    * remove unneeded builder exclusions from reimage test
    
    * allow supplying security group names in skeleton TF and use for smslabs
    
    * wait more for control node after reimage in CI
    
    * fix 4510bf331f373af4c8c9a05a659f573273af971e
    
    * try removing pause from CI reimage test
    
    * fix divide-by-zero bug in pingmatrix properly

--- environments/skeleton/{{cookiecutter.environment}}/terraform/getfaults.py
+++ environments/skeleton/{{cookiecutter.environment}}/terraform/getfaults.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Display any failure messages for openstack servers in current terraform state. """
-import json, subprocess
+import json, subprocess, os, sys
 
 def get_openstack_server(uuid):
     """ Return json with openstack server info """
@@ -8,15 +8,15 @@ def get_openstack_server(uuid):
     server_txt = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, universal_newlines=True).stdout
     return json.loads(server_txt)
 
-def read_tf_state():
-    """ Return json from terraform state in current directory """
+def read_tf_state(tf_dir):
+    """ Return json from terraform state file in `tf_dir` """
 
-    with open('terraform.tfstate') as statef:
+    with open(os.path.join(tf_dir, 'terraform.tfstate')) as statef:
         state = json.load(statef)
     return state
 
-def check_server_errors():
-    tf_state = read_tf_state()
+def check_server_errors(tf_dir):
+    tf_state = read_tf_state(tf_dir)
     for resource in tf_state['resources']:
         if resource['type'] == 'openstack_compute_instance_v2':
             for instance in resource['instances']:
@@ -29,4 +29,5 @@ def check_server_errors():
                     print(name, uuid, failure_msg)
 
 if __name__ == '__main__':
-    check_server_errors()
+    tf_dir = sys.argv[1] if len(sys.argv) > 1 else './'
+    check_server_errors(tf_dir)
