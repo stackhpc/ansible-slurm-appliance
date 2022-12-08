stackhpc/ansible-slurm-appliance

commit a51e55868af8eb7019aa540c2d6068f20ae0ae82

make arcus/CI volumes much smaller than default

environments/arcus/terraform/main.tf
@@ -17,7 +17,8 @@ variable "create_nodes" {
 variable "cluster_image" {
     description = "single image for all cluster nodes - a convenience for CI"
     type = string
-    default = "openhpc-221118-1422.qcow2" # https://github.com/stackhpc/slurm_image_builder/pull/12
+    # default = "openhpc-221118-1422.qcow2" # https://github.com/stackhpc/slurm_image_builder/pull/12
+    default = "Rocky-8-GenericCloud-8.6.20220702.0.x86_64.qcow2 "
 }
 
 module "cluster" {
@@ -56,5 +57,8 @@ module "cluster" {
     }
     create_nodes = var.create_nodes
     
+    state_volume_size = 10
+    home_volume_size = 10
+
     environment_root = var.environment_root
 }

commit 28d4adaead9da33d13a140429b4d96e06e2803e6

fix arcus/CI base image error introduced by a51e5586

environments/arcus/terraform/main.tf
@@ -17,8 +17,8 @@ variable "create_nodes" {
 variable "cluster_image" {
     description = "single image for all cluster nodes - a convenience for CI"
     type = string
-    # default = "openhpc-221118-1422.qcow2" # https://github.com/stackhpc/slurm_image_builder/pull/12
-    default = "Rocky-8-GenericCloud-8.6.20220702.0.x86_64.qcow2 "
+    default = "openhpc-221118-1422.qcow2" # https://github.com/stackhpc/slurm_image_builder/pull/12
+    #default = "Rocky-8-GenericCloud-8.6.20220702.0.x86_64.qcow2 "
 }
 
 module "cluster" {