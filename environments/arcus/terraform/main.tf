5b74c617218a61325f9a6d73ad40d3029d39c153
Steve Brasier <steveb@stackhpc.com>
2023-02-17 15:04:56 +0000

    bump fat image for arcus/ci to address fat image PR nits

--- environments/arcus/terraform/main.tf
+++ environments/arcus/terraform/main.tf
@@ -17,7 +17,7 @@ variable "create_nodes" {
 variable "cluster_image" {
     description = "single image for all cluster nodes - a convenience for CI"
     type = string
-    default = "openhpc-230215-1202.qcow2" # https://github.com/stackhpc/slurm_image_builder/pull/15
+    default = "openhpc-230217-1440.qcow2" # https://github.com/stackhpc/slurm_image_builder/pull/15
     # default = "Rocky-8-GenericCloud-Base-8.7-20221130.0.x86_64.qcow2"
     # default = "Rocky-8-GenericCloud-8.6.20220702.0.x86_64.qcow2"
 }
