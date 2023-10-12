888165711426d0c751aa99a915797401c31424ec
Steve Brasier <steveb@stackhpc.com>
2023-10-12 09:10:53 +0000

    use setup-env script to update galaxy installs

--- dev/setup-env.sh
+++ dev/setup-env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-/usr/bin/python3.8 -m venv venv # use `sudo yum install python38` on Rocky Linux 8 to install this
+if [[ ! -d "venv" ]]; then
+    /usr/bin/python3.8 -m venv venv # use `sudo yum install python38` on Rocky Linux 8 to install this
+fi
 . venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 ansible --version
-# Install ansible dependencies ...
-ansible-galaxy role install -r requirements.yml -p ansible/roles
-ansible-galaxy collection install -r requirements.yml -p ansible/collections
+# Install or update ansible dependencies ...
+ansible-galaxy role install -fr requirements.yml -p ansible/roles
+ansible-galaxy collection install -fr requirements.yml -p ansible/collections
