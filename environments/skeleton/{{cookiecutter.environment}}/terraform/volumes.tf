5d307b697cf755d57f89036c9931ec18e36635d0
Steve Brasier <steveb@stackhpc.com>
2023-12-08 12:20:31 +0000

    document volume label requirement

--- environments/skeleton/{{cookiecutter.environment}}/terraform/volumes.tf
+++ environments/skeleton/{{cookiecutter.environment}}/terraform/volumes.tf
@@ -1,11 +1,11 @@
 resource "openstack_blockstorage_volume_v3" "state" {
     name = "${var.cluster_name}-state"
-    description = "State for control node"
+    description = "State for control node" # first word used to label filesystem
     size = var.state_volume_size
 }
 
 resource "openstack_blockstorage_volume_v3" "home" {
     name = "${var.cluster_name}-home"
-    description = "Home for control node"
+    description = "Home for control node" # first word used to label filesystem
     size = var.home_volume_size
 }
