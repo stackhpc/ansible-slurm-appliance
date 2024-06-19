24c6b0563963167d4fc3bdb0f093e2bc86cc4c72
Steve Brasier <33413598+sjpb@users.noreply.github.com>
2024-06-19 14:37:34 +0100

    Disable grafana repos by default (#399)
    * disable grafana repos by default
    
    * bump CI image

--- environments/.stackhpc/terraform/main.tf
+++ environments/.stackhpc/terraform/main.tf
@@ -29,9 +29,9 @@ variable "cluster_image" {
     description = "single image for all cluster nodes, keyed by os_version - a convenience for CI"
     type = map(string)
     default = {
-        # https://github.com/stackhpc/ansible-slurm-appliance/pull/397
-        RL8: "openhpc-RL8-240606-1054-5ec8558e"
-        RL9: "openhpc-ofed-RL9-240606-1054-5ec8558e"
+        # https://github.com/stackhpc/ansible-slurm-appliance/pull/399
+        RL8: "openhpc-RL8-240619-0949-66c0e540"
+        RL9: "openhpc-ofed-RL9-240619-0949-66c0e540"
     }
 }
 
