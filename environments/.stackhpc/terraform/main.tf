21c6aacace40fb07645e97a39d30da529cea3ef8
Bertie <bertie@stackhpc.com>
2024-07-19 11:32:49 +0000

    update image

--- environments/.stackhpc/terraform/main.tf
+++ environments/.stackhpc/terraform/main.tf
@@ -29,9 +29,9 @@ variable "cluster_image" {
     description = "single image for all cluster nodes, keyed by os_version - a convenience for CI"
     type = map(string)
     default = {
-        # https://github.com/stackhpc/ansible-slurm-appliance/pull/410
-        RL8: "openhpc-RL8-240712-1426-6830f97b"
-        RL9: "openhpc-ofed-RL9-240712-1425-6830f97b"
+        # https://github.com/stackhpc/ansible-slurm-appliance/pull/411
+        RL8: "openhpc-RL8-240719-0943-1c8626e8"
+        RL9: "openhpc-ofed-RL9-240719-0943-1c8626e8"
     }
 }
 
