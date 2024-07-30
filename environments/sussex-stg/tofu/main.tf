4a810d1c25116383d757fecdd56e802c05e31edd
Scott Davidson <sd688@ceres.hrcm.susx.ac.uk>
2024-07-30 10:10:37 +0100

    Move a40 node defs to prod environment

--- environments/sussex-stg/tofu/main.tf
+++ environments/sussex-stg/tofu/main.tf
@@ -39,27 +39,5 @@ module "cluster" {
             "general-1",
           ]
       }
-      a40_1024 = {
-        flavor = "baremetal.r7525.128cpu.1tbram.a40"
-        image_id = "9fa9cc54-7447-4204-8d88-aaf0f314c648" # openhpc-extra-RL9-240723-1238-b5de8392
-        vnic_type = "baremetal"
-        nodes = [ # use -10 for 1TB nodes
-          "a40-10",
-          "a40-11",
-          "a40-12",
-          "a40-13",
-          "a40-14",
-         ]
-      }
-      a40_512 = {
-        flavor = "baremetal.r7525.128cpu.512gbram.a40"
-        image_id = "9fa9cc54-7447-4204-8d88-aaf0f314c648" # openhpc-extra-RL9-240723-1238-b5de8392
-        vnic_type = "baremetal"
-        nodes = [ # use -00 for 500GB nodes
-          "a40-00",
-          "a40-01",
-          "a40-02",
-        ]
-      }
     }
 }
