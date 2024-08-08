f7e77128035dc4842361e2409d39e443f6122da2
rocky Cloud User <cloud-user@bertie-deploy.novalocal>
2024-08-08 09:42:43 +0000

    Handle setup-env Python versions

--- dev/setup-env.sh
+++ dev/setup-env.sh
@@ -2,9 +2,33 @@
 
 set -euo pipefail
 
-if [[ ! -d "venv" ]]; then
-    /usr/bin/python3.9 -m venv venv # use `sudo yum install python38` on Rocky Linux 8 to install this
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS=$ID
+    VERSION_ID=$VERSION_ID
+else
+    exit 1
 fi
+
+MAJOR_VERSION=$(echo $VERSION_ID | cut -d. -f1)
+
+PYTHON_VERSION=""
+
+if [[ "$OS" == "ubuntu" && "$MAJOR_VERSION" == "22" ]]; then
+    PYTHON_VERSION="/usr/bin/python3.10"
+elif [[ "$OS" == "rocky" && "$MAJOR_VERSION" == "8" ]]; then
+    PYTHON_VERSION="/usr/bin/python3.8" # use `sudo yum install python38` on Rocky Linux 8 to install this
+elif [[ "$OS" == "rocky" && "$MAJOR_VERSION" == "9" ]]; then
+    PYTHON_VERSION="/usr/bin/python3.9"
+else
+    echo "Unsupported OS version: $OS $MAJOR_VERSION"
+    exit 1
+fi
+
+if [[ ! -d "venv" ]]; then
+    $PYTHON_VERSION -m venv venv
+fi
+
 . venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
