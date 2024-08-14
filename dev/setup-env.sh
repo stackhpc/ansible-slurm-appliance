73a4e5ed0ca2f1c2ddfa25a836ebd4b0c1bcf7eb
bertiethorpe <84867280+bertiethorpe@users.noreply.github.com>
2024-08-14 11:49:38 +0100

    Update setup-env.sh

--- dev/setup-env.sh
+++ dev/setup-env.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if [[ ! -d "venv" ]]; then
-    /usr/bin/python3 -m venv venv # use `sudo yum install python38` on Rocky Linux 8 to install this
+    /usr/bin/python3.8 -m venv venv # use `sudo yum install python38` on Rocky Linux 8 to install this
 fi
 . venv/bin/activate
 pip install -U pip
