706832d3963c31ed290715b7bd96e654fb19f93f
bertiethorpe <bertie443@gmail.com>
2024-08-22 10:50:48 +0000

    just use workflow_dispatch

--- .github/bin/get-s3-image.sh
+++ .github/bin/get-s3-image.sh
@@ -4,26 +4,23 @@ set -x
 
 # Variables (adjust these paths as necessary)
 S3_BUCKET="s3://openhpc-images"
-APPLIANCES_ENVIRONMENT_ROOT="$APPLIANCES_ENVIRONMENT_ROOT"
-MAIN_TF="$APPLIANCES_ENVIRONMENT_ROOT/terraform/main.tf"
 
 for IMAGE_OS in $IMAGE_LIST; do
-    echo "Extracting CI image name from $MAIN_TF"
-    ci_image=$(grep -oP 'openhpc-[0-9a-zA-Z-]+' "$MAIN_TF" | grep $IMAGE_OS)
+    
+    image_name=$1
+    echo "Checking if image $image_name exists in OpenStack"
+    image_exists=$(openstack image list --name "$image_name" -f value -c Name)
 
-    echo "Checking if image $ci_image exists in OpenStack"
-    image_exists=$(openstack image list --name "$ci_image" -f value -c Name)
-
-    if [ "$image_exists" == "$ci_image" ]; then
-        echo "Image $ci_image already exists in OpenStack."
+    if [ "$image_exists" == "$image_name" ]; then
+        echo "Image $image_name already exists in OpenStack."
     else
-        echo "Image $ci_image not found in OpenStack. Getting it from S3."
+        echo "Image $image_name not found in OpenStack. Getting it from S3."
 
-        wget https://object.arcus.openstack.hpc.cam.ac.uk/swift/v1/AUTH_3a06571936a0424bb40bc5c672c4ccb1/openhpc-images/$ci_image
+        wget https://object.arcus.openstack.hpc.cam.ac.uk/swift/v1/AUTH_3a06571936a0424bb40bc5c672c4ccb1/openhpc-images/$image_name
 
-        echo "Uploading image $ci_image to OpenStack..."
-        openstack image create --file "$ci_image.qcow2" --disk-format qcow2 "$ci_image"
+        echo "Uploading image $image_name to OpenStack..."
+        openstack image create --file "$image_name.qcow2" --disk-format qcow2 "$image_name"
 
-        echo "Image $ci_image has been uploaded to OpenStack."
+        echo "Image $image_name has been uploaded to OpenStack."
     fi
 done
