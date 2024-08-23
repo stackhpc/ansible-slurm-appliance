a5a1c0382d08db1d82fdde0cccc7aed057964a23
bertiethorpe <bertie443@gmail.com>
2024-08-23 10:56:07 +0000

    suggested changes

--- .github/bin/get-s3-image.sh
+++ .github/bin/get-s3-image.sh
@@ -12,7 +12,7 @@ bucket_name=$2
 echo "Checking if image $image_name exists in OpenStack"
 image_exists=$(openstack image list --name "$image_name" -f value -c Name)
 
-if [ "$image_exists" == "$image_name" ]; then
+if [ -n $image_exists ]; then
     echo "Image $image_name already exists in OpenStack."
 else
     echo "Image $image_name not found in OpenStack. Getting it from S3."
@@ -20,7 +20,7 @@ else
     wget https://object.arcus.openstack.hpc.cam.ac.uk/swift/v1/AUTH_3a06571936a0424bb40bc5c672c4ccb1/$bucket_name/$image_name --progress=dot:giga
 
     echo "Uploading image $image_name to OpenStack..."
-    openstack image create --file "$image_name" --disk-format qcow2 "$image_name" --progress
+    openstack image create --file $image_name --disk-format qcow2 $image_name --progress
 
     echo "Image $image_name has been uploaded to OpenStack."
 fi
