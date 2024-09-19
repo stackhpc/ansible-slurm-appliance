a8d4e173daae1fb197a9db424bd6e2c64eb35e14
wtripp180901 <will.tripp@btinternet.com>
2024-09-19 15:10:06 +0100

    k3s token now templated into terraform vars

--- environments/skeleton/{{cookiecutter.environment}}/terraform/compute/variables.tf
+++ environments/skeleton/{{cookiecutter.environment}}/terraform/compute/variables.tf
@@ -69,6 +69,5 @@ variable "security_group_ids" {
 }
 
 variable "k3s_token" {
-    description = "Random cryptographically secure string for K3s token (must be set by ../compute.tf)"
     type = string
 }
