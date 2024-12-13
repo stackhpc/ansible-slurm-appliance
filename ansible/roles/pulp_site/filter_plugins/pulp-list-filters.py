f0e48b90a36b16ff6e5c80740ab07587d5fcd467
wtripp180901 <will.tripp@btinternet.com>
2024-12-13 13:24:27 +0000

    pulp sync now mirrors upstream subpaths

--- ansible/roles/pulp_site/filter_plugins/pulp-list-filters.py
+++ ansible/roles/pulp_site/filter_plugins/pulp-list-filters.py
@@ -6,10 +6,10 @@ class FilterModule(object):
             'to_rpm_distros': self.to_rpm_distros
         }
 
-    def to_rpm_repos(self, list):
+    def to_rpm_repos(self, list, pulp_url):
         repo_list = map(lambda x: {
             'name': x['name'],
-            'url': x['url'],
+            'url': pulp_url+'/'+x['subpath'],
             'remote_username': x['remote_username'],
             'remote_password': x['remote_password'],
             'policy': x['policy'],
@@ -26,6 +26,6 @@ class FilterModule(object):
         distro_list = map(lambda x: {
             'name': x['name'],
             'repository': x['name'],
-            'base_path': x['base_path'],
+            'base_path': x['subpath'],
             'state': x['state'] }, list)
         return distro_list
