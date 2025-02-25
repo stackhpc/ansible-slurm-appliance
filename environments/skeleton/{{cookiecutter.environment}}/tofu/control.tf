30d6ce458368224399419be6e3f8701f33c9f059
Steve Brasier <33413598+sjpb@users.noreply.github.com>
2025-02-25 09:37:29 +0000

    support disabling port security (#592)

--- environments/skeleton/{{cookiecutter.environment}}/tofu/control.tf
+++ environments/skeleton/{{cookiecutter.environment}}/tofu/control.tf
@@ -14,7 +14,8 @@ resource "openstack_networking_port_v2" "control" {
     subnet_id = data.openstack_networking_subnet_v2.cluster_subnet[each.key].id
   }
 
-  security_group_ids = [for o in data.openstack_networking_secgroup_v2.nonlogin: o.id]
+  port_security_enabled = lookup(each.value, "port_security_enabled", true)
+  security_group_ids = lookup(each.value, "port_security_enabled", true) ? [for o in data.openstack_networking_secgroup_v2.nonlogin: o.id] : []
 
   binding {
     vnic_type = lookup(var.vnic_types, each.key, "normal")
