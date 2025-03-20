fd221ba40d0a30ca65066cfe29dc788113b3153b
Pierre Riteau <pierre@stackhpc.com>
2025-03-20 13:36:48 +0100

    Import OpenTofu for production

--- environments/production/tofu/main.tf
+++ environments/production/tofu/main.tf
@@ -1,3 +1,83 @@
+variable "environment_root" {
+    type = string
+    description = "Path to environment root, automatically set by activate script"
+}
+
 module "cluster" {
-    source = "../../site/tofu/"
+  source = "../../site/tofu/"
+
+  cluster_name = "io"
+  cluster_networks = [
+    {
+      network = "netapp"
+      subnet = "netapp"
+    }
+  ]
+  key_pair = "rally"
+  control_node_flavor = "m1.xlarge"
+  login = {
+    # Arbitrary group name for these login nodes
+    interactive = {
+      nodes = [
+        "login-01",
+        "login-02",
+      ]
+      flavor = "m1.xlarge"
+      fip_addresses = [
+        "194.199.232.101",
+        "194.199.232.102",
+      ]
+    }
+  }
+  # RL9.4 + OFED 24.10-1.1.4.0 + SSSD
+  cluster_image_id = "479747da-bfcd-4aec-81f0-8c9475e2d461"
+  compute = {
+    # Group name used for compute node partition definition
+    cpu = {
+      flavor = "baremetal"
+      nodes = [
+        "io-cpu-01",
+        "io-cpu-02",
+      ]
+      extra_networks = [
+        {
+          network = "infiniband"
+          subnet = "infiniband"
+          no_security_groups = true
+        }
+      ]
+      match_ironic_node = true
+      vnic_types = {
+        netapp = "baremetal"
+        infiniband = "baremetal"
+      }
+    }
+    gpu = {
+      flavor = "baremetal-gpu"
+      # RL9.4 + OFED 24.10-1.1.4.0 + SSSD + CUDA 12.8
+      image_id = "527eb511-ee34-4c8d-9391-2ca101668022"
+      nodes = [
+        "io-gpu-01",
+        "io-gpu-02",
+        "io-gpu-03",
+        "io-gpu-04",
+        "io-gpu-05",
+        "io-gpu-06",
+      ]
+      extra_networks = [
+        {
+          network = "infiniband"
+          subnet = "infiniband"
+          no_security_groups = true
+        }
+      ]
+      match_ironic_node = true
+      vnic_types = {
+        netapp = "baremetal"
+        infiniband = "baremetal"
+      }
+    }
+  }
+
+  environment_root = var.environment_root
 }
