3694bb61ba5569aa28d45604f14f86f8e7042f9e
Steve Brasier <steveb@stackhpc.com>
2025-04-08 14:23:14 +0000

    add cluster_nodename_template and compute/login.nodename_template

--- environments/skeleton/{{cookiecutter.environment}}/tofu/main.tf
+++ environments/skeleton/{{cookiecutter.environment}}/tofu/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.7" # templatestring() function
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
