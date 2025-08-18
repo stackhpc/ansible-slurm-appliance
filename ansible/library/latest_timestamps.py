c8635bba5b27061784221a8f4d9fd452a260d12b
wtripp180901 <will.tripp@btinternet.com>
2025-08-18 09:43:47 +0100

    updated timestamps script + bumped rocky 9 timestamps

--- ansible/library/latest_timestamps.py
+++ ansible/library/latest_timestamps.py
@@ -52,18 +52,21 @@ def run_module():
     )
 
     timestamps = dict(module.params['repos_dict'])
-    for repo in timestamps:
-        for version in timestamps[repo]:
+    for index in timestamps:
+        for repo in timestamps[index]:
+            for version in timestamps[index][repo]:
 
-            html_txt = requests.get(
-                    url= module.params['content_url'] + '/' + timestamps[repo][version]['path']
-                ).text
-            timestamp_link_list = BeautifulSoup(html_txt,features="html.parser").body.find('pre').find_all() # getting raw list of timestamps from html
-            timestamp_link_list = map(lambda x: x.string,timestamp_link_list) # stripping xml tags
-            latest_timestamp = list(timestamp_link_list)[-1][:-1] # last timestamp in list with trailing / removed
-            timestamps[repo][version]['timestamp'] = latest_timestamp
+                html_txt = requests.get(
+                        url= module.params['content_url'] + '/' + timestamps[index][repo][version]['pulp_path']
+                    ).text
+                timestamp_link_list = BeautifulSoup(html_txt,features="html.parser").body.find('pre').find_all() # getting raw list of timestamps from html
+                timestamp_link_list = map(lambda x: x.string,timestamp_link_list) # stripping xml tags
+                latest_timestamp = list(timestamp_link_list)[-1][:-1] # last timestamp in list with trailing / removed
+                timestamps[index][repo][version]['pulp_timestamp'] = latest_timestamp
+        # timestamps[index] = dict(sorted(timestamps[index]))
             
-    result['timestamps'] = dict(sorted(timestamps.items()))
+    
+    result['timestamps'] = timestamps
 
     module.exit_json(**result)
 
