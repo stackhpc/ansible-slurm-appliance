275da838fa3727cfd745d733573d04527df70198
wtripp180901 <78219569+wtripp180901@users.noreply.github.com>
2025-09-04 13:47:32 +0100

    Refactor Pulp repo definitions and add more Pulp documentation (#760)
    * WIP: refactor repos definitions
    
    * add more repos and cope with CRB/PowerTools oddness
    
    * add epel
    
    * use pulp_server as a group
    
    * add epel default
    
    * wip: get pulp sync working
    
    * fixed sync
    
    * autodetect latest in adhoc script, refactored timestamps to allow gated ohpc repos, fixed pulp site
    
    * fixed distributions + ohpc repos
    
    * updated timestamps script + bumped rocky 9 timestamps
    
    * removed pulp_repo_name fields
    
    * updated docs, added gpg checks, simplified filters
    
    * Added pulp systemd file + removed unused vars
    
    * added READMEs + updated variable names
    
    * disabled gpg checks for dnf_repos
    
    * typo
    
    * fixed disable repos task
    
    * bump images
    
    * remove dnf_repos extra index/key and make epel/openhpc special-cases simpler
    
    * clarify pulp distro selection
    
    * fixup sync vars
    
    * fixup grafana vars
    
    * revert latest timestamp changes for extra key level
    
    * review suggestions
    
    Co-authored-by: Steve Brasier <33413598+sjpb@users.noreply.github.com>
    
    * updated README
    
    * docs tweaks
    
    * regularised group names
    
    * updated operations guide for functionality requiring additional installs
    
    * review changes from docs
    
    Co-authored-by: Steve Brasier <33413598+sjpb@users.noreply.github.com>
    
    * renamed timestamps.yml to dnf_repos_timestamps.yml
    
    ---------
    
    Co-authored-by: Steve Brasier <steveb@stackhpc.com>
    Co-authored-by: Steve Brasier <33413598+sjpb@users.noreply.github.com>

--- ansible/roles/pulp_site/filter_plugins/pulp-list-filters.py
+++ ansible/roles/pulp_site/filter_plugins/pulp-list-filters.py
@@ -3,29 +3,61 @@ class FilterModule(object):
         return {
             'to_rpm_repos': self.to_rpm_repos,
             'to_rpm_pubs': self.to_rpm_pubs,
-            'to_rpm_distros': self.to_rpm_distros
+            'to_rpm_distros': self.to_rpm_distros,
+            'select_repos': self.select_repos,
         }
-
-    def to_rpm_repos(self, list, pulp_url):
-        repo_list = map(lambda x: {
-            'name': x['name'],
-            'url': pulp_url+'/'+x['subpath'],
-            'remote_username': x['remote_username'],
-            'remote_password': x['remote_password'],
-            'policy': x['policy'],
-            'state': x['state'] }, list)
-        return repo_list
     
+    def select_repos(self, dnf_repos, target_distro_ver):
+        """ Filter dnf_repos to only those for a relevant distribution version (M.m or M). Returns a list of dicts.
+            Also adds pulp_repo_name field to give the repository a unique name in Pulp to be referenced by subsequent
+            filters
+        """
+    
+        target_distro_ver_major = target_distro_ver.split('.')[0]
+
+        rpm_repos = []
+        for repokey in dnf_repos:
+            # select either the matching major.minor or major version:
+            if target_distro_ver in dnf_repos[repokey]:
+                selected_ver = target_distro_ver
+            elif target_distro_ver_major in dnf_repos[repokey]:
+                selected_ver = target_distro_ver_major
+            else:
+                raise ValueError(f'No key matching {target_distro_ver_major} or {target_distro_ver} found in f{repokey}')
+            repo_data = dnf_repos[repokey][selected_ver]
+            repo_data['pulp_repo_name'] = f"{repokey}-{selected_ver}-{dnf_repos[repokey][selected_ver]['pulp_timestamp']}"
+            rpm_repos.append(repo_data)
+        return rpm_repos
+
+    def to_rpm_repos(self, rpm_info, content_url, repo_defaults):
+        """ Filter repo object list given by select_repos into dict required by the pulp_repository_rpm_repos variable
+            from stackhpc.pulp.pulp_repository role
+        """
+        rpm_repos = []
+        for repo_data in rpm_info:
+            rpm_data = repo_defaults.copy() # NB: this changes behaviour vs before, so now defaults can correctly be overriden
+            rpm_data['name'] = repo_data['pulp_repo_name']
+            rpm_data['url'] = '/'.join([content_url, repo_data['pulp_path'], repo_data['pulp_timestamp']])
+            rpm_data['state'] = 'present'
+            rpm_repos.append(rpm_data)
+        return rpm_repos
+
     def to_rpm_pubs(self, list):
+        """ Filter repo object list given by select_repos into dict required by the pulp_publication_rpm variable
+            from stackhpc.pulp.pulp_publication role
+        """
         pub_list = map(lambda x: {
-            'repository': x['name'],
-            'state': x['state'] }, list)
+            'repository': x['pulp_repo_name'],
+            'state': 'present' }, list)
         return pub_list
     
     def to_rpm_distros(self, list):
+        """ Filter repo object list given by select_repos into dict required by the pulp_distirubtion_rpm variable
+            from stackhpc.pulp.pulp_distribution role
+        """
         distro_list = map(lambda x: {
-            'name': x['name'],
-            'repository': x['name'],
-            'base_path': x['subpath'],
-            'state': x['state'] }, list)
+            'name': x['pulp_repo_name'],
+            'repository': x['pulp_repo_name'],
+            'base_path': '/'.join([x['pulp_path'],x['pulp_timestamp']]),
+            'state': 'present' }, list)
         return distro_list
