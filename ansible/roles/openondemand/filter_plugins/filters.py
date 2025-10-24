4e6ce198b6baf1acc484ee17e46b7b15bb18d14b
Steve Brasier <steveb@stackhpc.com>
2025-10-24 17:18:33 +0100

    fix lint errors

--- ansible/roles/openondemand/filter_plugins/filters.py
+++ ansible/roles/openondemand/filter_plugins/filters.py
@@ -32,7 +32,7 @@ def to_gres_options(stdout):
     gres_data = {}
     # key=gres_opt - 'name' or 'name:type', i.e. what would be passed to --gres
     # value={label:str, max_count: int, partitions=[]}
-    gres_data["none"] = {'label':'None', 'max_count':0, 'partitions':['all']}
+    gres_data["none"] = {"label": "None", "max_count": 0, "partitions": ["all"]}
 
     for line in stdout.splitlines():
         # line examples:
@@ -41,7 +41,7 @@ def to_gres_options(stdout):
         # - First example shows multiple GRES per partition
         # - Core suffix e.g. '(S:0-1)' only exists for auto-detected gres
         # - stackhpc.openhpc role guarantees that name:type:count all exist
-        partition, gres_definitions = (line.split())
+        partition, gres_definitions = line.split()
         for gres in gres_definitions.split(","):
             if "(null)" in gres:
                 continue
@@ -54,17 +54,21 @@ def to_gres_options(stdout):
                         if ":" in gres_opt
                         else f"Any {gres_opt}"
                     )
-                    gres_data[gres_opt] = {'label':label, 'max_count':gres_count, 'partitions':[partition]}
+                    gres_data[gres_opt] = {
+                        "label": label,
+                        "max_count": gres_count,
+                        "partitions": [partition],
+                    }
                 else:
-                    gres_data[gres_opt]['partitions'].append(partition)
-                    if gres_count > gres_data[gres_name]['max_count']:
-                        gres_data[gres_opt]['max_count'] = gres_count
+                    gres_data[gres_opt]["partitions"].append(partition)
+                    if gres_count > gres_data[gres_name]["max_count"]:
+                        gres_data[gres_opt]["max_count"] = gres_count
 
     gres_options = []
     for gres_opt in gres_data:  # pylint: disable=consider-using-dict-items
-        max_count = gres_data[gres_opt]['max_count']
-        partitions = gres_data[gres_opt]['partitions']
-        label = gres_data[gres_opt]['label']
+        max_count = gres_data[gres_opt]["max_count"]
+        partitions = gres_data[gres_opt]["partitions"]
+        label = gres_data[gres_opt]["label"]
         if gres_opt != "none":
             label += f" (max count={max_count}, partitions={','.join(partitions)})"
         gres_options.append((label, gres_opt))
