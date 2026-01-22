95e8d015d9be74058c5294995a713b68a1a9babf
Steve Brasier <steveb@stackhpc.com>
2026-01-22 11:28:53 +0000

    simplify subnet TF inventory var

--- environments/site/tofu/inventory.tf
+++ environments/site/tofu/inventory.tf
@@ -1,4 +1,7 @@
 # tflint-ignore: terraform_required_providers
+locals {
+  subnet_keys = ["name", "cidr"]
+}
 resource "local_file" "hosts" {
   content = templatefile("${path.module}/inventory.tpl",
     {
@@ -11,6 +14,7 @@ resource "local_file" "hosts" {
       "additional_groups" : module.additional
       "state_dir" : var.state_dir
       "cluster_home_volume" : var.home_volume_provisioning != "none"
+      "cluster_subnets": [for net in var.cluster_networks: {for k, v in data.openstack_networking_subnet_v2.cluster_subnet[net.network]: k => v if contains(local.subnet_keys, k)}]
     },
   )
   filename = "../inventory/hosts.yml"
