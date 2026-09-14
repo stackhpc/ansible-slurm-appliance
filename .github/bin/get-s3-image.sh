stackhpc/ansible-slurm-appliance

commit d8466f646f8239d3a7d015d8ee9c5eb526b3e1fa

Run syft in the packer VM and reference sbom in openstack image property (#1020)
* Run syft inside the packer VM during fatimage build

Copy it to the runner via ansible
Sites can add builder to the sbom group to also run syft on
their derived images.

* Upload sbom to s3 with metadata , add property to image

Store branch SBOMs to a separate bucket, to be able to purge often.

* Download sbom in scan

Verify image checksum is consistent with SBOM during scan

* Change to relative locations in .grype.yaml

Needed since syft scan in image (I haven't found a way to generate
absolute paths with syft scan in image)

* Use and copy metadata and sbom when syncing and releasing images

* Store github run producing the image in glance and S3

* Clarify comment about If-None-Match s3cmd limitations

Co-authored-by: Steve Brasier <33413598+sjpb@users.noreply.github.com>

* Rebuilt images

https://github.com/stackhpc/ansible-slurm-appliance/actions/runs/34458364575

* Fix issue with curl -I endlines are CR-LF not just LF

---------

Co-authored-by: Steve Brasier <33413598+sjpb@users.noreply.github.com>

.github/bin/get-s3-image.sh
@@ -16,11 +16,28 @@ if [ -n "$image_exists" ]; then
   echo "Image $image_name already exists in OpenStack."
 else
   echo "Image $image_name not found in OpenStack. Getting it from S3."
+  image_url="https://leafcloud.store/swift/v1/AUTH_f39848421b2747148400ad8eeae8d536/$bucket_name/$image_name"
+  wget "$image_url" --progress=dot:giga
 
-  wget "https://leafcloud.store/swift/v1/AUTH_f39848421b2747148400ad8eeae8d536/$bucket_name/$image_name" --progress=dot:giga
+  image_id=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-image-id | sed 's/.*://;s/\r//')
+  image_sha512=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-image-sha512 | sed 's/.*://;s/\r//')
+  qcow2_sha512=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-qcow2-sha512 | sed 's/.*://;s/\r//')
+  sbom_url=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-sbom-url | sed 's/.*://')
+
+  sha512sum -c <<<"$qcow2_sha512 $image_name"
 
   echo "Uploading image $image_name to OpenStack..."
   openstack image create --file "$image_name" --disk-format qcow2 "$image_name" --progress
-
   echo "Image $image_name has been uploaded to OpenStack."
+  openstack image set \
+    --property "stackhpc_sbom=$sbom_url" \
+    --property "stackhpc_original_image_id=$image_id" \
+    --property "stackhpc_original_image_sha512=$image_sha512" \
+    "$image_name" || true
+
+  target_image_sha512="$(openstack image show -f json -c properties "${image_name}" | jq -r '.properties.os_hash_value')"
+  if [ "$target_image_sha512" != "$qcow2_sha512" ]; then
+    echo "Uploaded Image $image_name in QCOW2 format has unexpected $target_image_sha512 vs expected $qcow2_sha512"
+    exit 1
+  fi
 fi

.github/workflows/release-image.yml
@@ -6,6 +6,7 @@ name: Release images
       - published # should work for both pre-releases and releases
 env:
   IMAGE_PATH: environments/.stackhpc/tofu/cluster_image.auto.tfvars.json
+  S3_BUCKET_PUBLIC_URL: https://leafcloud.store/swift/v1/AUTH_f39848421b2747148400ad8eeae8d536/openhpc-images
 
 permissions: {}
 
@@ -40,5 +41,12 @@ jobs:
           TARGET_IMAGE=$(jq --arg version "${{ matrix.build }}" -r '.cluster_image[$version]' "${{ env.IMAGE_PATH }}")
           echo "TARGET_IMAGE=${TARGET_IMAGE}" >> "$GITHUB_ENV"
 
-      - name: Copy image from pre-release to release bucket
-        run: s3cmd cp "s3://openhpc-images-prerelease/${TARGET_IMAGE}" s3://openhpc-images
+      - name: Copy SBOM and image from pre-release to release bucket
+        run: |
+          sbom_filename="${TARGET_IMAGE}.syft-json.zip"
+          sbom_url="${S3_BUCKET_PUBLIC_URL}/${sbom_filename}"
+          # TODO: confirm s3cmd cp copies metadata and overrides with the one we set manually
+          s3cmd cp "s3://openhpc-images-prerelease/${TARGET_IMAGE}.syft-json.zip" s3://openhpc-images
+          s3cmd cp \
+            --add-header "x-amz-meta-stackhpc-sbom-url: $sbom_url" \
+            "s3://openhpc-images-prerelease/${TARGET_IMAGE}" s3://openhpc-images

.github/workflows/s3-image-sync.yml
@@ -8,6 +8,7 @@ name: Upload CI-tested images to Leafcloud S3 and sync clouds
       - 'environments/.stackhpc/tofu/cluster_image.auto.tfvars.json'
 env:
   S3_BUCKET: openhpc-images-prerelease
+  S3_BUCKET_PUBLIC_URL: https://leafcloud.store/swift/v1/AUTH_f39848421b2747148400ad8eeae8d536/openhpc-images-prerelease
   IMAGE_PATH: environments/.stackhpc/tofu/cluster_image.auto.tfvars.json
   RUNNER_IMAGE_DIR: /mnt/images
 
@@ -97,18 +98,41 @@ jobs:
       - name: Retrieve image name
         run: |
           TARGET_IMAGE=$(jq --arg version "$MATRIX_BUILD" -r '.cluster_image[$version]' "$IMAGE_PATH")
-          echo "TARGET_IMAGE=${TARGET_IMAGE}" >> "$GITHUB_ENV"
+          echo "TARGET_IMAGE=${TARGET_IMAGE}" | tee -a "$GITHUB_ENV" "$GITHUB_STEP_SUMMARY"
         shell: bash
         env:
           MATRIX_BUILD: ${{ matrix.build }}
 
-      - name: Download image to runner
+      - name: Download image and SBOM to runner
         run: |
           . venv/bin/activate
           df -h
           sudo mkdir "$RUNNER_IMAGE_DIR"
           sudo chmod ugo=rwX "$RUNNER_IMAGE_DIR"
+
+          sbom_url="$(openstack image show --format json "${TARGET_IMAGE}" | jq -r '.properties.stackhpc_sbom // ""')"
+          if [ -n "$sbom_url" ] && [[ "$sbom_url" == https://leafcloud.store* ]]; then
+            echo Downloading "$sbom_url..."
+            wget -nv "$sbom_url" -O "${RUNNER_IMAGE_DIR}/sbom.zip"
+          else
+            echo "Missing or invalid sbom_url='$sbom_url' (quotes added)"
+            exit 1
+          fi
+          TARGET_IMAGE_SHA512="$(openstack image show -f json -c properties "${TARGET_IMAGE}" | jq -r '.properties.os_hash_value')"
+          TARGET_IMAGE_GITHUB_RUN="$(openstack image show -f json -c properties "${TARGET_IMAGE}" | jq -r '.properties.stackhpc_github_run')"
+          SBOM_IMAGE_SHA512=$(curl -sS -I "$sbom_url" | grep x-object-meta-stackhpc-image-checksum | sed 's/.*sha512://;s/\r//')
+          SBOM_GITHUB_RUN=$(curl -sS -I "$sbom_url" | grep x-object-meta-github-run | sed 's/.*://;s/\r//')
+          SBOM_IMAGE_ID=$(curl -sS -I "$sbom_url" | grep x-object-meta-stackhpc-image-id | sed 's/.*://;s/\r//')
+          if [ "$TARGET_IMAGE_SHA512" != "$SBOM_IMAGE_SHA512" ]; then
+            echo "E: Image $TARGET_IMAGE checksum in OpenStack is $TARGET_IMAGE_SHA512 but recorded with sbom is $SBOM_IMAGE_SHA512"
+            exit 1
+          fi
+          echo "SBOM_IMAGE_SHA512=$SBOM_IMAGE_SHA512" | tee -a "$GITHUB_ENV" "$GITHUB_STEP_SUMMARY"
+          echo "SBOM_GITHUB_RUN=$SBOM_GITHUB_RUN" | tee -a "$GITHUB_ENV" "$GITHUB_STEP_SUMMARY"
+          echo "SBOM_IMAGE_ID=$SBOM_IMAGE_ID" | tee -a "$GITHUB_ENV" "$GITHUB_STEP_SUMMARY"
+          echo "TARGET_IMAGE_GITHUB_RUN=$TARGET_IMAGE_GITHUB_RUN" | tee -a "$GITHUB_ENV" "$GITHUB_STEP_SUMMARY"
           openstack image save --file "${RUNNER_IMAGE_DIR}/${TARGET_IMAGE}.raw" "${TARGET_IMAGE}"
+          sha512sum -c <<<"${SBOM_IMAGE_SHA512} ${RUNNER_IMAGE_DIR}/${TARGET_IMAGE}.raw"
           df -h
         shell: bash
 
@@ -120,8 +144,25 @@ jobs:
 
       - name: Upload Image to S3
         run: |
+          qcow2_image_sha512=$(sha512sum "${RUNNER_IMAGE_DIR}/${TARGET_IMAGE}" | awk '{ print $1 }')
+          sbom_filename="${TARGET_IMAGE}.syft-json.zip"
+          sbom_url="${S3_BUCKET_PUBLIC_URL}/${sbom_filename}"
           echo "Uploading Image: ${TARGET_IMAGE} to S3..."
-          s3cmd --multipart-chunk-size-mb=150 put "${RUNNER_IMAGE_DIR}/${TARGET_IMAGE}" "s3://${S3_BUCKET}"
+          s3cmd --multipart-chunk-size-mb=150 put \
+            --add-header "x-amz-meta-github-run: $TARGET_IMAGE_GITHUB_RUN" \
+            --add-header "x-amz-meta-stackhpc-image-id: $SBOM_IMAGE_ID" \
+            --add-header "x-amz-meta-stackhpc-image-name: $TARGET_IMAGE" \
+            --add-header "x-amz-meta-stackhpc-image-sha512: $SBOM_IMAGE_SHA512" \
+            --add-header "x-amz-meta-stackhpc-qcow2-sha512: $qcow2_image_sha512" \
+            --add-header "x-amz-meta-stackhpc-sbom-url: $sbom_url" \
+            "${RUNNER_IMAGE_DIR}/${TARGET_IMAGE}" "s3://${S3_BUCKET}"
+          echo "Uploading SBOM: ${TARGET_IMAGE}.syft-json.zip to S3..."
+          s3cmd put \
+            --add-header "x-amz-meta-github-run: $SBOM_GITHUB_RUN" \
+            --add-header "x-amz-meta-stackhpc-image-id: $SBOM_IMAGE_ID" \
+            --add-header "x-amz-meta-stackhpc-image-name: $TARGET_IMAGE" \
+            --add-header "x-amz-meta-stackhpc-image-sha512: $SBOM_IMAGE_SHA512" \
+            "${RUNNER_IMAGE_DIR}/${TARGET_IMAGE}/sbom.zip" "s3://${S3_BUCKET}/${sbom_filename}"
         shell: bash
 
   image_sync:

.github/workflows/fatimage.yml
@@ -34,10 +34,10 @@ jobs:
         build:
           - image_name: openhpc-RL8
             source_image_name: Rocky-8-GenericCloud-Base-8.10-20240528.0.x86_64.qcow2
-            inventory_groups: fatimage
+            inventory_groups: fatimage,sbom
           - image_name: openhpc-RL9
             source_image_name: Rocky-9-GenericCloud-Base-9.8-20260525.0.x86_64
-            inventory_groups: fatimage
+            inventory_groups: fatimage,sbom
     env:
       ANSIBLE_FORCE_COLOR: 'true'
       OS_CLOUD: openstack
@@ -45,6 +45,8 @@ jobs:
       ARK_PASSWORD: ${{ secrets.ARK_PASSWORD }}
       PACKER_GITHUB_API_TOKEN: ${{ secrets.GITHUB_TOKEN }}
       PACKER_ON_ERROR: ${{ github.event.inputs.cleanup_on_failure == 'true' && 'cleanup' || 'abort' }}
+      S3_BUCKET: openhpc-branch-sboms
+      S3_BUCKET_PUBLIC_URL: https://leafcloud.store/swift/v1/AUTH_f39848421b2747148400ad8eeae8d536/openhpc-branch-sboms
 
     steps:
       - uses: actions/checkout@df4cb1c069e1874edd31b4311f1884172cec0e10 # v6.0.3
@@ -147,8 +149,10 @@ jobs:
             sleep 5
           done
           IMAGE_NAME=$(openstack image show -f value -c name "$IMAGE_ID")
+          IMAGE_CHECKSUM="$(openstack image show -f json -c properties "$IMAGE_ID" | jq -r '"\(.properties.os_hash_algo):\(.properties.os_hash_value)"')"
           echo "image-name=${IMAGE_NAME}" | tee -a "$GITHUB_OUTPUT" "$GITHUB_STEP_SUMMARY"
           echo "image-id=$IMAGE_ID" | tee -a "$GITHUB_OUTPUT" "$GITHUB_STEP_SUMMARY"
+          echo "image-checksum=$IMAGE_CHECKSUM" | tee -a "$GITHUB_OUTPUT" "$GITHUB_STEP_SUMMARY"
           echo "$IMAGE_ID" > image-id.txt
           echo "$IMAGE_NAME" > image-name.txt
 
@@ -158,6 +162,40 @@ jobs:
           openstack image unset --property signature_verified "${STEPS_MANIFEST_OUTPUTS_IMAGE_ID}" || true
         env:
           STEPS_MANIFEST_OUTPUTS_IMAGE_ID: ${{ steps.manifest.outputs.image-id }}
+
+      - name: Write s3cmd configuration
+        run: |
+          echo "$SECRETS_LEAFCLOUD_S3_CFG" > ~/.s3cfg
+        shell: bash
+        env:
+          SECRETS_LEAFCLOUD_S3_CFG: ${{ secrets.LEAFCLOUD_S3_CFG }}
+
+      - name: Install s3cmd
+        run: |
+          sudo apt-get update
+          sudo apt-get --yes install s3cmd
+
+      - name: Upload SBOM
+        run: |
+          . venv/bin/activate
+          sbom_filename="${STEPS_MANIFEST_OUTPUTS_IMAGE_NAME}.syft-json.zip"
+          sbom_url="${S3_BUCKET_PUBLIC_URL}/${sbom_filename}"
+          run_url="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+          # Uses If-None-Match header to prevent over-writing - this does not work in multipart mode
+          s3cmd  put \
+            --add-header 'If-None-Match: *' --disable-multipart \
+            --add-header "x-amz-meta-github-run: $run_url" \
+            --add-header "x-amz-meta-stackhpc-image-id: $STEPS_MANIFEST_OUTPUTS_IMAGE_ID" \
+            --add-header "x-amz-meta-stackhpc-image-name: $STEPS_MANIFEST_OUTPUTS_IMAGE_NAME" \
+            --add-header "x-amz-meta-stackhpc-image-checksum: $STEPS_MANIFEST_OUTPUTS_IMAGE_CHECKSUM" \
+            sbom.zip "s3://$S3_BUCKET/${sbom_filename}"
+          echo "sbom-url=${sbom_url}" | tee -a "$GITHUB_OUTPUT" "$GITHUB_STEP_SUMMARY"
+          openstack image set --property "stackhpc_sbom=$sbom_url" "${STEPS_MANIFEST_OUTPUTS_IMAGE_ID}" || true
+          openstack image set --property "stackhpc_github_run=$run_url" "${STEPS_MANIFEST_OUTPUTS_IMAGE_ID}" || true
+        env:
+          STEPS_MANIFEST_OUTPUTS_IMAGE_ID: ${{ steps.manifest.outputs.image-id }}
+          STEPS_MANIFEST_OUTPUTS_IMAGE_NAME: ${{ steps.manifest.outputs.image-name }}
+          STEPS_MANIFEST_OUTPUTS_IMAGE_CHECKSUM: ${{ steps.manifest.outputs.image-checksum }}
 
       - name: Set image properties
         run: |

.github/workflows/scan.yml
@@ -73,8 +73,7 @@ jobs:
           IMAGE_NAME=$(jq --arg version "$MATRIX_BUILD" -r '.cluster_image[$version]' "$JSON_PATH")
           echo "IMAGE_NAME=${IMAGE_NAME}" >> "$GITHUB_ENV"
           echo "image-name=${IMAGE_NAME}" | tee -a "$GITHUB_OUTPUT" "$GITHUB_STEP_SUMMARY"
-          echo "SBOM_NAME=${IMAGE_NAME}-sbom.syft-json" >> "$GITHUB_ENV"
-          echo "sbom_name=${IMAGE_NAME}-sbom.syft-json" >> "$GITHUB_OUTPUT"
+          echo "sbom_name=${IMAGE_NAME}.syft-json" | tr '[:upper:]' '[:lower:]' | tee -a "$GITHUB_ENV" "$GITHUB_OUTPUT"
         env:
           MATRIX_BUILD: ${{ matrix.build }}
           JSON_PATH: ${{ env.JSON_PATH }}
@@ -86,42 +85,32 @@ jobs:
           path: ${{ steps.manifest.outputs.sbom_name }}
           key: ${{ steps.manifest.outputs.sbom_name }}
 
-      - name: Download image
-        # NB: use /mnt as that is a separate disk with more space
+      - name: Download image SBOM
         if: steps.cache_restore.outputs.cache-hit != 'true'
         run: |
           . venv/bin/activate
-          sudo mkdir /mnt/images
-          sudo chmod 777 /mnt/images
-          openstack image save --file "/mnt/images/${IMAGE_NAME}" "${IMAGE_NAME}"
-
-      - name: Set up QEMU
-        if: steps.cache_restore.outputs.cache-hit != 'true'
-        uses: docker/setup-qemu-action@ce360397dd3f832beb865e1373c09c0e9f86d70a # v4.0.0
-
-      - name: Install libguestfs
-        if: steps.cache_restore.outputs.cache-hit != 'true'
-        run: |
-          sudo apt -y update
-          sudo apt -y install libguestfs-tools
-
-      - name: Make directory for mount
-        if: steps.cache_restore.outputs.cache-hit != 'true'
-        run: sudo mkdir -p "./${IMAGE_NAME}"
-
-      - name: Mount image file
-        if: steps.cache_restore.outputs.cache-hit != 'true'
-        run: sudo guestmount -a "/mnt/images/${IMAGE_NAME}" -i --ro -o allow_other "./${IMAGE_NAME}"
-
-      - name: Generate sbom and attach to release if applicable
-        if: steps.cache_restore.outputs.cache-hit != 'true'
-        uses: anchore/sbom-action@e22c389904149dbc22b58101806040fa8d37a610 # v0.24.0
-        id: generate-sbom
-        with:
-          path: "./${{ steps.manifest.outputs.image-name }}"
-          artifact-name: ${{ steps.manifest.outputs.sbom_name }}
-          output-file: ${{ steps.manifest.outputs.sbom_name }}
-          format: syft-json
+          sbom_url="$(openstack image show --format json "${IMAGE_NAME}" | jq -r '.properties.stackhpc_sbom // ""')"
+          # be slightly robust to string injection
+          if [ -n "$sbom_url" ] && [[ "$sbom_url" == https://leafcloud.store* ]] && [[ "$sbom_url" == *syft-json.zip ]]; then
+            TARGET_IMAGE_SHA512="$(openstack image show -f json -c properties "${IMAGE_NAME}" | jq -r '.properties.os_hash_value')"
+            SBOM_IMAGE_SHA512=$(curl -sS -I "$sbom_url" | grep x-object-meta-stackhpc-image-checksum | sed 's/.*sha512://;s/\r//')
+            if [ "$TARGET_IMAGE_SHA512" = "$SBOM_IMAGE_SHA512" ]; then
+              echo "Image checksum matches SBOM: $TARGET_IMAGE_SHA512"
+            else
+              echo "E: Image $IMAGE_NAME checksum in OpenStack is '$TARGET_IMAGE_SHA512' but recorded with sbom is '$SBOM_IMAGE_SHA512'"
+              exit 1
+            fi
+            echo Downloading "$sbom_url..."
+            wget -nv "$sbom_url" -O sbom.zip
+            if ! unzip sbom.zip "${sbom_name}" || [ ! -e "${sbom_name}" ]; then
+              echo "Unexpected content in sbom zip: didn't produce ${sbom_name}"
+              unzip -l sbom.zip
+              exit 1
+            fi
+          else
+            echo "Missing or invalid sbom_url='$sbom_url' (quotes added)"
+            exit 1
+          fi
 
       - name: Save SBOM cache
         if: steps.cache_restore.outputs.cache-hit != 'true'

ansible/cleanup.yml
@@ -40,37 +40,6 @@
 - name: Cleanup /tmp # noqa: no-changed-when
   ansible.builtin.shell: rm -rvf /tmp/*
 
-- name: Delete files triggering vulnerability scans
-  ansible.builtin.file:
-    path: "{{ item }}"
-    state: absent
-  loop: # NB: items here MUST have a justification!
-    # ondemand install: raised at https://github.com/OSC/ondemand/security/advisories/GHSA-f7j8-ppqm-m5vw
-    # All declared not to be an issue by Open Ondemand as relevant packages not installed
-    - "/opt/ood/ondemand/root/usr/share/gems/3.3/ondemand/{{ ondemand_package_version }}-1/gems/bootstrap_form-2.7.0/test/dummy/Gemfile.lock"
-    - "/opt/ood/ondemand/root/usr/share/gems/3.3/ondemand/{{ ondemand_package_version }}-1/gems/bootstrap_form-5.0.0/demo/yarn.lock"
-    - /var/www/ood/apps/sys/dashboard/node_modules/data-confirm-modal/Gemfile.lock
-    # chrony role: only used for role dev, venv never created on disk
-    - /etc/ansible-init/playbooks/roles/mrlesmithjr.chrony/poetry.lock
-    - /etc/ansible-init/playbooks/roles/mrlesmithjr.chrony/requirements.txt
-    - /etc/ansible-init/playbooks/roles/mrlesmithjr.chrony/requirements-dev.txt
-    # netbox collection: only used for role dev
-    - /usr/lib/ansible-init/lib/python3.9/site-packages/ansible_collections/netbox/netbox/poetry.lock
-    # esbuild in OOD
-    - /var/www/ood/apps/sys/dashboard/node_modules/@esbuild/linux-x64/bin/esbuild
-    - /usr/lib/rstudio-server/bin/quarto/bin/tools/x86_64/esbuild
-
-- name: Replace rclone with a no-op script
-  ansible.builtin.copy:
-    content: |
-      #!/bin/sh
-      echo "The rclone executable has been removed in ansible-slurm-appliance images due to too many security warnings." 1>&2
-      exit 1
-    dest: /usr/bin/rclone
-    owner: root
-    group: root
-    mode: "0555"
-
 - name: Stop journald
   # Stop journald from writing data to the journal after we have cleared it.
   ansible.builtin.service:

ansible/fatimage.yml
@@ -281,6 +281,46 @@
 
 - import_playbook: final.yml
 
+- name: Cleanup dubious files with respect to security
+  hosts: builder
+  become: true
+  gather_facts: true
+  tags: sbom
+  tasks:
+    - name: Cleanup /tmp # noqa: no-changed-when
+      ansible.builtin.shell: rm -rvf /tmp/*
+    - name: Delete files triggering vulnerability scans
+      ansible.builtin.file:
+        path: "{{ item }}"
+        state: absent
+      loop: # NB: items here MUST have a justification!
+        # ondemand install: raised at https://github.com/OSC/ondemand/security/advisories/GHSA-f7j8-ppqm-m5vw
+        # All declared not to be an issue by Open Ondemand as relevant packages not installed
+        - "/opt/ood/ondemand/root/usr/share/gems/3.3/ondemand/{{ ondemand_package_version }}-1/gems/bootstrap_form-2.7.0/test/dummy/Gemfile.lock"
+        - "/opt/ood/ondemand/root/usr/share/gems/3.3/ondemand/{{ ondemand_package_version }}-1/gems/bootstrap_form-5.0.0/demo/yarn.lock"
+        - /var/www/ood/apps/sys/dashboard/node_modules/data-confirm-modal/Gemfile.lock
+        # chrony role: only used for role dev, venv never created on disk
+        - /etc/ansible-init/playbooks/roles/mrlesmithjr.chrony/poetry.lock
+        - /etc/ansible-init/playbooks/roles/mrlesmithjr.chrony/requirements.txt
+        - /etc/ansible-init/playbooks/roles/mrlesmithjr.chrony/requirements-dev.txt
+        # netbox collection: only used for role dev
+        - /usr/lib/ansible-init/lib/python3.9/site-packages/ansible_collections/netbox/netbox/poetry.lock
+        # esbuild in OOD
+        - /var/www/ood/apps/sys/dashboard/node_modules/@esbuild/linux-x64/bin/esbuild
+        - /usr/lib/rstudio-server/bin/quarto/bin/tools/x86_64/esbuild
+    - name: Replace rclone with a no-op script
+      ansible.builtin.copy:
+        content: |
+          #!/bin/sh
+          echo "The rclone executable has been removed in ansible-slurm-appliance images due to too many security warnings." 1>&2
+          exit 1
+        dest: /usr/bin/rclone
+        owner: root
+        group: root
+        mode: "0555"
+
+- import_playbook: generate-sbom.yml
+
 - hosts: builder
   become: true
   gather_facts: true

commit 717d893925580f74273a799697b0bcb659c921b9

Fix remaining space in some values in s3-image-sync (#1038)
Fixes issues introduced by #1020

.github/bin/get-s3-image.sh
@@ -19,10 +19,10 @@ else
   image_url="https://leafcloud.store/swift/v1/AUTH_f39848421b2747148400ad8eeae8d536/$bucket_name/$image_name"
   wget "$image_url" --progress=dot:giga
 
-  image_id=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-image-id | sed 's/.*://;s/\r//')
-  image_sha512=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-image-sha512 | sed 's/.*://;s/\r//')
-  qcow2_sha512=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-qcow2-sha512 | sed 's/.*://;s/\r//')
-  sbom_url=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-sbom-url | sed 's/.*://')
+  image_id=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-image-id | sed 's/.*: //;s/\r//')
+  image_sha512=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-image-sha512 | sed 's/.*: //;s/\r//')
+  qcow2_sha512=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-qcow2-sha512 | sed 's/.*: //;s/\r//')
+  sbom_url=$(curl -sS -I "$image_url" | grep x-object-meta-stackhpc-sbom-url | sed 's/.*: //;s/\r//')
 
   sha512sum -c <<<"$qcow2_sha512 $image_name"
 

.github/workflows/release-image.yml
@@ -45,7 +45,7 @@ jobs:
         run: |
           sbom_filename="${TARGET_IMAGE}.syft-json.zip"
           sbom_url="${S3_BUCKET_PUBLIC_URL}/${sbom_filename}"
-          # TODO: confirm s3cmd cp copies metadata and overrides with the one we set manually
+          # s3cmd cp copies metadata and overrides with the one we set manually
           s3cmd cp "s3://openhpc-images-prerelease/${TARGET_IMAGE}.syft-json.zip" s3://openhpc-images
           s3cmd cp \
             --add-header "x-amz-meta-stackhpc-sbom-url: $sbom_url" \

.github/workflows/s3-image-sync.yml
@@ -121,8 +121,8 @@ jobs:
           TARGET_IMAGE_SHA512="$(openstack image show -f json -c properties "${TARGET_IMAGE}" | jq -r '.properties.os_hash_value')"
           TARGET_IMAGE_GITHUB_RUN="$(openstack image show -f json -c properties "${TARGET_IMAGE}" | jq -r '.properties.stackhpc_github_run')"
           SBOM_IMAGE_SHA512=$(curl -sS -I "$sbom_url" | grep x-object-meta-stackhpc-image-checksum | sed 's/.*sha512://;s/\r//')
-          SBOM_GITHUB_RUN=$(curl -sS -I "$sbom_url" | grep x-object-meta-github-run | sed 's/.*://;s/\r//')
-          SBOM_IMAGE_ID=$(curl -sS -I "$sbom_url" | grep x-object-meta-stackhpc-image-id | sed 's/.*://;s/\r//')
+          SBOM_GITHUB_RUN=$(curl -sS -I "$sbom_url" | grep x-object-meta-github-run | sed 's/.*: //;s/\r//')
+          SBOM_IMAGE_ID=$(curl -sS -I "$sbom_url" | grep x-object-meta-stackhpc-image-id | sed 's/.*: //;s/\r//')
           if [ "$TARGET_IMAGE_SHA512" != "$SBOM_IMAGE_SHA512" ]; then
             echo "E: Image $TARGET_IMAGE checksum in OpenStack is $TARGET_IMAGE_SHA512 but recorded with sbom is $SBOM_IMAGE_SHA512"
             exit 1